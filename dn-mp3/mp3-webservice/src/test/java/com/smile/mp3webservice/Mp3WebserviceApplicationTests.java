2b4b60856d04a2c5e065413e07d80f2bdf59e6db
utthao <thaoduyendao98@gmail.com>
2019-08-29 14:39:47 +0700

    design-partern

---
+++ dn-mp3/mp3-webservice/src/test/java/com/smile/mp3webservice/Mp3WebserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.smile.mp3webservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mp3WebserviceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
