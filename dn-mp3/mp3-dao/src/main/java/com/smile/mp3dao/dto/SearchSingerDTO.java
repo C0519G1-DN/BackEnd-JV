c4be5d7a4a147aee9610d1265ad51a9b242cf9b8
nguyennhatthanh <nguyennhatthanh3394@gmail.com>
2019-09-20 17:53:04 +0700

    commit add singer on song 2

---
+++ dn-mp3/mp3-dao/src/main/java/com/smile/mp3dao/dto/SearchSingerDTO.java
@@ -0,0 +1,13 @@
+package com.smile.mp3dao.dto;
+
+public class SearchSingerDTO {
+    private String nameSinger;
+
+    public String getNameSinger() {
+        return nameSinger;
+    }
+
+    public void setNameSinger(String nameSinger) {
+        this.nameSinger = nameSinger;
+    }
+}
